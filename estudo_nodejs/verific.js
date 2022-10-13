071683469868df9cccc4df5298dc6cc3e3a164a4
junior <junim.rafael@gmail.com>
2022-10-13 01:35:55 -0300

    Verificação

--- estudo_nodejs/verific.js
+++ estudo_nodejs/verific.js
@@ -1,24 +1,43 @@
 import fetch from "node-fetch";
 
+function handle(erro){
+    throw new Error(erro.message);
+}
+
 async function status(arrayLink){
-    //promisse async await
-    const arrayStatus = await Promise.all(arrayLink.map(async url => {
-        const res = await fetch(url);
-        return res.status;
-    }))
-    return arrayStatus;
+    try{
+        //promisse async await
+        const arrayStatus = await Promise
+            .all(arrayLink 
+                .map(async url => {
+                    const res = await fetch(url);
+                    return res.status;
+        }))
+        return arrayStatus;
+    } catch(erro){
+        handle(erro);
+    }
+
 
 }
 
 function joinUrlArray(arrayLink){
     //join tira todos os arrays de dentro e coloca em um só.
-    return arrayLink.map(objeto => (Object.values(objeto).join()));
+    return arrayLink
+        .map(objeto => (Object
+            .values(objeto).join()));
 }
 
 async function verifyUrl(arrayLink){
     const links = joinUrlArray(arrayLink);
     const statusLinks = await status(links);
-    return statusLinks;
+    //spread operator
+    const resultados = arrayLink
+        .map((objeto, index) => ({
+            ...objeto, 
+            status:statusLinks[index]
+        }))
+    return resultados;
 }
 
 export {verifyUrl}
